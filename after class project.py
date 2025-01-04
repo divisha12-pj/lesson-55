62e550dcd845e4f2fef46ece6a5e39c83c023cfe
divishajain12-pj <divishajain1210@gmail.com>
2025-01-04 22:10:23 +0530

    project

---
+++ after class project.py
@@ -0,0 +1,16 @@
+def mul(n,m):
+    total=n*m
+    print("\n1 iterations :" ,total)
+
+    #for N iterations run a for loop
+    
+    def mul(n,m):
+       total=0
+       for i in range(1,n+1):
+           total+=1
+    print("N iteration : ",total)
+
+n=int(input("enter a for a*b "))
+m=int(input("enter b for a*b "))
+mul(n,m)
+mull(n,m)
